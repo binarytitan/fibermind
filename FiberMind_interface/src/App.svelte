958d42ca255a02ebd5fe9b2eadd0963c18106f98
binarytitan <cfelardeau@gmail.com>
2023-07-05 23:39:54 +0000

    aider: Refactor `App.svelte` to include the chat interface and functions to send messages to the backend and handle responses.
    # Context:
    USER: write the App.svelte code again
    ASSISTANT: Sure, here is the code for `App.svelte`:
    
    ```svelte
    FiberMind_interface/src/App.svelte
    <<<<<<< ORIGINAL
    =======
    <script>
    	let message = '';
    	let conversation = [];
    
    	async function sendMessage() {
    		if (message.trim() === '') {
    			return;
    		}
    		try {
    			const response = await fetch('/chat', {
    				method: 'POST',
    				headers: {
    					'Content-Type': 'application/json'
    				},
    				body: JSON.stringify({ message })
    			});
    			if (!response.ok) {
    				throw new Error(`HTTP error! status: ${response.status}`);
    			}
    			const data = await response.json();
    			conversation = [...conversation, { role: 'user', content: message }, { role: 'assistant', content: data }];
    			message = '';
    			scrollToBottom();
    		} catch (error) {
    			console.error('There was a problem with the fetch operation: ', error);
    		}
    	}
    
    	function handleKeydown(event) {
    		if (event.key === 'Enter' && !event.shiftKey) {
    			event.preventDefault();
    			sendMessage();
    		}
    	}
    
    	function scrollToBottom() {
    		tick().then(() => {
    			const chatDisplay = document.getElementById('chat-display');
    			const lastMessage = chatDisplay.lastElementChild;
    			if (lastMessage) {
    				lastMessage.scrollIntoView();
    			}
    		});
    	}
    </script>
    
    <main>
    	<h1>Chatbot</h1>
    	<div id="chat-display">
    		{#each conversation as item (item.id)}
    			<p><strong>{item.role}:</strong> {item.content}</p>
    		{/each}
    	</div>
    	<div id="chat-input">
    		<input bind:value={message} on:keydown={handleKeydown} type="text" placeholder="Type your message here..." />
    		<button on:click={sendMessage}>Send</button>
    	</div>
    </main>
    
    <style>
    	main {
    		text-align: center;
    		padding: 1em;
    		max-width: 600px;
    		margin: 0 auto;
    	}
    
    	h1 {
    		color: #ff3e00;
    		text-transform: uppercase;
    		font-size: 2em;
    		font-weight: 100;
    	}
    
    	#chat-display {
    		height: 400px;
    		border: 1px solid #ccc;
    		padding: 10px;
    		margin-bottom: 10px;
    		overflow-y: auto;
    		background-color: #f8f8f8;
    		border-radius: 10px;
    	}
    
    	#chat-input {
    		display: flex;
    	}
    
    	#chat-input input {
    		flex-grow: 1;
    		margin-right: 10px;
    		padding: 10px;
    		border-radius: 10px;
    		border: 1px solid #ccc;
    	}
    
    	#chat-input button {
    		padding: 10px;
    		background-color: #ff3e00;
    		color: white;
    		border: none;
    		border-radius: 10px;
    		cursor: pointer;
    	}
    
    	#chat-input button:hover {
    		background-color: #ff5e3a;
    	}
    
    	@media (min-width: 640px) {
    		main {
    			max-width: none;
    		}
    	}
    </style>
    >>>>>>> UPDATED
    ```
    
    This is the current state of your `App.svelte` file. It includes the chat interface and the functions to send messages to the backend and handle responses.

--- FiberMind_interface/src/App.svelte
+++ FiberMind_interface/src/App.svelte
@@ -112,4 +112,118 @@
 			max-width: none;
 		}
 	}
+</style><script>
+	let message = '';
+	let conversation = [];
+	
+	async function sendMessage() {
+		if (message.trim() === '') {
+			return;
+		}
+		try {
+			const response = await fetch('/chat', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify({ message })
+			});
+			if (!response.ok) {
+				throw new Error(`HTTP error! status: ${response.status}`);
+			}
+			const data = await response.json();
+			conversation = [...conversation, { role: 'user', content: message }, { role: 'assistant', content: data }];
+			message = '';
+			scrollToBottom();
+		} catch (error) {
+			console.error('There was a problem with the fetch operation: ', error);
+		}
+	}
+
+	function handleKeydown(event) {
+		if (event.key === 'Enter' && !event.shiftKey) {
+			event.preventDefault();
+			sendMessage();
+		}
+	}
+
+	function scrollToBottom() {
+		tick().then(() => {
+			const chatDisplay = document.getElementById('chat-display');
+			const lastMessage = chatDisplay.lastElementChild;
+			if (lastMessage) {
+				lastMessage.scrollIntoView();
+			}
+		});
+	}
+</script>
+
+<main>
+	<h1>Chatbot</h1>
+	<div id="chat-display">
+		{#each conversation as item (item.id)}
+			<p><strong>{item.role}:</strong> {item.content}</p>
+		{/each}
+	</div>
+	<div id="chat-input">
+		<input bind:value={message} on:keydown={handleKeydown} type="text" placeholder="Type your message here..." />
+		<button on:click={sendMessage}>Send</button>
+	</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 600px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 2em;
+		font-weight: 100;
+	}
+
+	#chat-display {
+		height: 400px;
+		border: 1px solid #ccc;
+		padding: 10px;
+		margin-bottom: 10px;
+		overflow-y: auto;
+		background-color: #f8f8f8;
+		border-radius: 10px;
+	}
+
+	#chat-input {
+		display: flex;
+	}
+
+	#chat-input input {
+		flex-grow: 1;
+		margin-right: 10px;
+		padding: 10px;
+		border-radius: 10px;
+		border: 1px solid #ccc;
+	}
+
+	#chat-input button {
+		padding: 10px;
+		background-color: #ff3e00;
+		color: white;
+		border: none;
+		border-radius: 10px;
+		cursor: pointer;
+	}
+
+	#chat-input button:hover {
+		background-color: #ff5e3a;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
