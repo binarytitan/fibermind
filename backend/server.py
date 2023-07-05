5abdb2e47b50ae941594f9c160b39e5f50f7ceac
binarytitan <cfelardeau@gmail.com>
2023-07-05 18:08:03 -0400

    aider: Added a new route `/chat` to the Flask server that accepts POST requests and handles chat functionality using GPT-3.5-turbo.
    # Context:
    USER: I added these *read-write* files: backend/server.py
    ASSISTANT: Great! Now that we have a basic Flask server running, let's add a new route that will handle the chat functionality. We'll call this route `/chat`. This route will accept POST requests, which will contain the user's message to the chatbot. The server will then process this message, generate a response using GPT-3.5-turbo, and send this response back to the client.
    
    Here's how you can add this new route to your server:
    
    ```python
    backend\server.py
    <<<<<<< ORIGINAL
    from flask import Flask, request, jsonify
    
    app = Flask(__name__)
    
    @app.route('/')
    def home():
        return "Chatbot Server is running!"
    
    if __name__ == '__main__':
        app.run(debug=True)
    =======
    from flask import Flask, request, jsonify
    from openai import ChatCompletion
    
    app = Flask(__name__)
    
    @app.route('/')
    def home():
        return "Chatbot Server is running!"
    
    @app.route('/chat', methods=['POST'])
    def chat():
        message = request.json['message']
        chat_model = "gpt-3.5-turbo"
        messages = [
            {"role": "system", "content": "You are a helpful assistant."},
            {"role": "user", "content": message},
        ]
        response = ChatCompletion.create(model=chat_model, messages=messages)
        return jsonify(response['choices'][0]['message']['content'])
    
    if __name__ == '__main__':
        app.run(debug=True)
    >>>>>>> UPDATED
    ```
    
    This code does the following:
    
    1. Imports the `ChatCompletion` class from the `openai` module.
    2. Defines a new route (`/chat`) that accepts POST requests.
    3. In this route, it first gets the user's message from the request.
    4. It then uses this message to generate a response from the chatbot using the `ChatCompletion.create` method.
    5. Finally, it sends this response back to the client.
    
    You can test this route by sending a POST request to `http://localhost:5000/chat` with a JSON body that looks like this: `{"message": "Hello, chatbot!"}`. You should get a response from the chatbot.
    
    Let me know if you have any questions about this part, or if you're ready to move on to the next step.

--- backend/server.py
+++ backend/server.py
@@ -1,10 +1,22 @@
 from flask import Flask, request, jsonify
+from openai import ChatCompletion
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Chatbot Server is running!"
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    message = request.json['message']
+    chat_model = "gpt-3.5-turbo"
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": message},
+    ]
+    response = ChatCompletion.create(model=chat_model, messages=messages)
+    return jsonify(response['choices'][0]['message']['content'])
 
 if __name__ == '__main__':
     app.run(debug=True)
